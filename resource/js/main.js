c5dfa5e073b9478ce315ee82e840f580178f161e
world-one <worldone.dev@gmail.com>
2021-10-10 16:15:19 +0900

    refact: main.js

--- resource/js/main.js
+++ resource/js/main.js
@@ -2,62 +2,71 @@ import Drawing from './draw.js';
 import { start } from './ball.js';
 import Heartbeat from './heartbeat.js';
 
-window.onload = (e) => {
-  setCanvas();
-  const hash = e.path[0].location.hash;
-  if (hash === '#drawing') {
-    new Drawing();
-  } else if (hash === '#bounceBall') {
-      start();
-  } else {
-    new Drawing();
-  }
+const URL_HASH = {
+  DRAWING: '#drawing',
+  BOUNCE_BALL: '#bounceBall',
+  HEARTBEAT: '#heartbeat',
 };
 
-const menu = document.querySelector('.menu');
-menu.addEventListener('click', (e) => {
-  const hash = e.target.hash;
-  if (hash === '#drawing') {
-    changeCanvas();
-    new Drawing();
-  } else if (hash === '#bounceBall') {
-    changeCanvas();
-    start();
+const CANVAS_ID = 'canvas';
 
-  } else if (hash === '#heartbeat') {
-    changeCanvas();
-    new Heartbeat();
+window.onload = (e) => {
+  setMenu();
+  selectedCanvas(e.path[0].location.hash);
+};
+
+function setMenu() {
+  const menu = document.querySelector('.menu');
+  menu.addEventListener('click', (e) => selectedCanvas(e.target.hash));
+};
+
+function selectedCanvas(hash) {
+  changeCanvas();
+
+  switch (hash) {
+    case URL_HASH.DRAWING:
+      new Drawing();
+      break;
+    case URL_HASH.BOUNCE_BALL:
+      start();
+      break;
+    case URL_HASH.HEARTBEAT:
+      new Heartbeat();
+      break;
+    default:
+      start();
+      break;
   }
-  
-});
+};
 
 function changeCanvas() {
   removeContentChildren();
   setCanvas();
-}
+};
 
 function removeContentChildren() {
   const contentsWrap = document.getElementById('Contents');
   while (contentsWrap.hasChildNodes()) {
     contentsWrap.removeChild(contentsWrap.firstChild);
   }
-}
+};
 
 function setCanvas() {
   const contentsWrap = document.getElementById('Contents');
   const canvas = document.createElement('canvas');
-  canvas.id = 'canvas';
+  canvas.id = CANVAS_ID;
   const ctx = canvas.getContext('2d');
   contentsWrap.appendChild(canvas);
   resize();
-}
+};
 
 function resize() {
-  const canvas = document.getElementById('canvas');
-  canvas.width = window.innerWidth * 0.9 * 0.8;
-  canvas.height = window.innerHeight * 0.8;
-  window.addEventListener('resize', (e) => {
-    canvas.width = e.target.innerWidth * 0.9 * 0.8;
-    canvas.height = e.target.innerHeight * 0.8;
-  });
-}
+  setCanvasSize(window);
+  window.addEventListener('resize', (e) => setCanvasSize(e.target));
+};
+
+function setCanvasSize(wrap) {
+  const canvas = document.getElementById(CANVAS_ID);
+  canvas.width = wrap.innerWidth * 0.9 * 0.8;
+  canvas.height = wrap.innerHeight * 0.8;
+};
