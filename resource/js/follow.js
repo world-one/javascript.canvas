5c729b57fb9650c7d8354f00ec142d925876098b
world-one <worldone.dev@gmail.com>
2021-10-19 22:29:29 +0900

    follow ball smooth

--- resource/js/follow.js
+++ resource/js/follow.js
@@ -1,38 +1,51 @@
 export function follow() {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
-
-  const draw = ({ x, y }) => {
-    ctx.beginPath();
-    ctx.fillStyle = 'blue';
-    ctx.arc(x, y, 25, Math.PI * 2, false);
-    ctx.closePath();
-    ctx.fill();
+  const canvas_width = canvas.width;
+  const canvas_height = canvas.height;
+  let ballX = canvas.width / 2;
+  let ballY = canvas.height / 2;
+  let pageX = 0;
+  let pageY = 0;
+  init();
+  function init() {
+    draw();
   }
 
-  const setPosition = (e) => {
+  
+  function draw () {
+    ctx.clearRect(0, 0, canvas_width, canvas_height);
+    ctx.beginPath();
+    ctx.fillStyle = 'blue';
+    ctx.arc(ballX, ballY, 25, Math.PI * 2, false);
+    ctx.closePath();
+    ctx.fill();
+    setBallPosition();
+    requestAnimationFrame(draw);
+  }
+
+  
+  
+  canvas.addEventListener('mousemove', (e) => {
+    const [x, y] = setPosition(e);
+    pageX = x;
+    pageY = y;
+    const canvas_width = canvas.width;
+    const canvas_height = canvas.height;
+    draw();
+  });
+
+  function setBallPosition() {
+    ballX = ballX + (pageX - ballX) / 10000;
+    ballY = ballY + (pageY - ballY) / 10000;
+  }
+
+  function setPosition (e) {
     return [ 
       e.pageX - canvas.offsetLeft,
       e.pageY - canvas.offsetTop 
     ];
   }
-  
-  canvas.addEventListener('mousemove', (e) => {
-    const [x, y] = setPosition(e);
-    const canvas_width = canvas.width;
-    const canvas_height = canvas.height;
-    
-    setTimeout(() => {
-      ctx.clearRect(0, 0, canvas_width, canvas_height);
-      draw({x, y});
-    }, 100)
-    // draw({x, y});
-  });
-
-
-  // return {
-  //   draw
-  // }
 }
 
 export default follow
